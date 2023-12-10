938065f12a54a7c73441caa9e6cbbcacd786a403
paoloose <pflores.fisi22@gmail.com>
2023-12-10 16:57:04 -0500

    day3(2023): finish part 2

--- 2023/3_Gear_Ratios/src/part2.rs
+++ 2023/3_Gear_Ratios/src/part2.rs
@@ -4,16 +4,42 @@ use crate::common::{EngineView, SymbolPosition};
 
 impl<'a> EngineView<'a> {
     fn next_gear(&mut self) -> Option<SymbolPosition> {
+        if self.src.is_empty() { return None; }
         let mut chars = self.src[self.nline].chars().skip(self.nchar);
 
-        while let Some(c) = chars.next() {
-            self.nchar += 1;
-            if c == '*' {
-                return Some(SymbolPosition(self.nchar - 1));
+        loop {
+            match chars.next() {
+                Some(c) => {
+                    self.nchar += 1;
+                    if c == '*' {
+                        return Some(SymbolPosition(self.nchar - 1));
+                    }
+                },
+                None => {
+                    self.nline += 1;
+                    self.nchar = 0;
+                    if self.nline == self.src.len() { return None; }
+                    chars = self.src[self.nline].chars().skip(0);
+                },
             }
         }
+    }
 
-        None
+    fn line_up(&self) -> Option<&str> {
+        if self.src.is_empty() { return None }
+        if self.nline == 0 { return None }
+        Some(self.src[self.nline - 1])
+    }
+
+    fn line_down(&self) -> Option<&str> {
+        if self.src.is_empty() { return None }
+        if self.nline == self.src.len() - 1 { return None }
+        Some(self.src[self.nline + 1])
+    }
+
+    fn current_line(&self) -> Option<&str> {
+        if self.src.is_empty() { return None }
+        Some(self.src[self.nline])
     }
 }
 
@@ -48,7 +74,8 @@ fn extract_nums_from_slice(line: &str, range: RangeInclusive<usize>) -> Vec<u32>
     let mut x = *range.start();
     let end = *range.end();
 
-    while x < end {
+    while x <= end {
+        // println!("extracting at pos {x} -> {c}", c=line.as_bytes()[x] as char);
         if let Some(ranged_num) = extract_num_at_pos(line, x) {
             let num = &line[ranged_num.clone()];
             nums.push(num.parse::<u32>().unwrap());
@@ -58,17 +85,30 @@ fn extract_nums_from_slice(line: &str, range: RangeInclusive<usize>) -> Vec<u32>
             x += 1;
         }
     }
-
     nums
 }
 
 pub fn solution() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("./day3.input")?;
     let mut engine = EngineView::new(&input);
+    let mut sum = 0;
 
-    let nums = extract_nums_from_slice("...*..*....................328.........../...460.325.....................+...........*.........198.....-.316...............912..............", 1..=50);
+    while let Some(gear) = engine.next_gear() {
+        let range = gear.0.checked_sub(1).unwrap_or(0)..=gear.0.checked_add(1).unwrap_or(gear.0);
 
-    dbg!(nums);
+        let nums_up = engine.line_up()
+            .map_or(vec![], |l| extract_nums_from_slice(l, range.clone()));
+        let nums_curr = engine.current_line()
+            .map_or(vec![], |l| extract_nums_from_slice(l, range.clone()));
+        let nums_down = engine.line_down()
+            .map_or(vec![], |l| extract_nums_from_slice(l, range.clone()));
 
+        let nums = [nums_up, nums_curr, nums_down].concat();
+        if nums.len() == 2 {
+            sum += nums[0] * nums[1];
+        }
+    }
+
+    println!("sum: {}", sum);
     Ok(())
 }
