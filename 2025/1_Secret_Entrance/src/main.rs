57ba760376936e8b4776504f4b4ecff3efe0b7c7
Paolo Flores <pflores.fisi22@gmail.com>
2025-12-09 16:56:45 -0500

    add day1 part2

--- 2025/1_Secret_Entrance/src/main.rs
+++ 2025/1_Secret_Entrance/src/main.rs
@@ -6,7 +6,7 @@ fn rotate_right(current: u32, amount: u32) -> u32 {
     (current + amount) % 100
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn part1() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("input.txt")?;
     let input = input.trim();
 
@@ -37,6 +37,64 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("current = {current}");
     println!("zeroes = {zeroes}");
+
+    Ok(())
+}
+
+fn part2() -> Result<(), Box<dyn Error>> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let input = input.trim();
+
+    let mut current = START_NUM;
+
+    let mut zeroes = 0;
+
+    for part in input.split("\n") {
+        let (direction_str, amount_str) = part.split_at(1);
+        println!("-> {direction_str}{amount_str}");
+        let mut amount_num = amount_str.parse::<u32>()?;
+
+        while amount_num >= 100 {
+            amount_num -= 100;
+            zeroes += 1;
+        }
+
+        amount_num = {
+            if amount_num == 0 {
+                amount_num
+            }
+            else if direction_str == "L" {
+                if (amount_num >= current) && current != 0 {
+                    zeroes += 1;
+                }
+                100 - amount_num
+            }
+            else if direction_str == "R" {
+                if (amount_num + current >= 100) && current != 0 {
+                    zeroes += 1;
+                }
+                amount_num
+            }
+            else {
+                unreachable!("malformed input")
+            }
+        };
+        println!("-> R{amount_num}");
+
+        current = rotate_right(current, amount_num);
+
+        println!("current: {current}");
+        println!("zeroes: {zeroes}");
+        println!();
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // part1();
+
+    part2()?;
 
     Ok(())
 }
